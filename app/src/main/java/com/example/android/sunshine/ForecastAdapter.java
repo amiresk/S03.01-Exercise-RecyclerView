7c2a780d603f1e9b51399881d898b78fffed04a7
amiresk <amiresg@gmx.net>
2017-01-22 16:44:05 -0800

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,78 @@
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by amhoes on 1/22/17.
+ */
+
+
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+
+
+    private String[] mWeatherData;
+
+    public ForecastAdapter(){
+
+    }
+
+
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+
+
+        public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View view){
+
+            super(view);
+            mWeatherTextView  = (TextView)view.findViewById(R.id.tv_weather_data);
+
+        }
+
+
+    }
+
+
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+
+        Context context = viewGroup.getContext();
+        int layoutForListItem = R.layout.forecast_list_item;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediatly = false;
+
+        View view = inflater.inflate(layoutForListItem, viewGroup, shouldAttachToParentImmediatly);
+
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder forecastAdapterViewHolder, int position) {
+
+        String weatherForThisDay = mWeatherData[position];
+        forecastAdapterViewHolder.mWeatherTextView.setText(weatherForThisDay);
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        if(null == mWeatherData) return 0;
+        return mWeatherData.length;
+    }
+
+    public void setWeatherData(String[] weatherData){
+
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+
+
+}
